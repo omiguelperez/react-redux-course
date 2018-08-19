6d876008f1aef73499040275e44844e66e670919
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-08-19 11:32:02 -0500

    Purse and State components

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 'use strict'
 
-class HelloComponent extends React.Component {
+/* Pure Component with UI */
+function HelloComponent (props) {
+  return <h1 id="title" onClick={props.handleClick}>
+    Hello {props.name} {props.count}
+  </h1>
+}
+
+/* State component with Logic */
+class MyComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { count: 0 }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  
   render () {
-    const name = this.props.name
-    return <h1 id="title">Hello {name}</h1>
+    return <HelloComponent
+      name={this.props.name}
+      count={this.state.count}
+      handleClick={this.handleClick}
+    />
   }
 }
 
 ReactDOM.render(
-  <HelloComponent name="Oscar" />,
+  <MyComponent name="Oscar" />,
   document.getElementById('app')
 )
