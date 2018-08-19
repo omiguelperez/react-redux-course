8d482f14b8086fd6e6d690eb63d940416be4e5a1
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-08-19 12:05:25 -0500

    Components lifecycle methos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const container = document.getElementById('app')
+
 /* Pure Component with UI */
 function HelloComponent (props) {
   return <h1 id="title" onClick={props.handleClick}>
@@ -13,6 +15,46 @@ class MyComponent extends React.Component {
     super(props)
     this.state = { count: 0 }
     this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentWillMount () {
+    console.log('El componente se va a montar!')
+  }
+
+  componentDidMount () {
+    console.log('El componente se ha montado!')
+    document.addEventListener('click', this.handleClick)
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('Componente recibe propiedades y estados', nextProps, nextState)
+
+    const { name } = this.props
+    const { count } = this.state
+    
+    /* Update state count after click  */
+    if (name === nextProps.name && count !== nextState.count)
+      return true
+    
+    /* Update name after 2 seconds */
+    if (name !== nextProps.name && count === nextState.count) {
+      this.state.count *= 2
+      return true
+    }
+    
+    return false
+  }
+
+  getSnapshotBeforeUpdate (prevProps, prevState) {
+    console.log('Componente antes de actualizar', prevProps, prevState)
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    console.log('Componente ha sido actualizado', prevProps, prevState)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.handleClick)
   }
 
   handleClick () {
@@ -32,5 +74,13 @@ class MyComponent extends React.Component {
 
 ReactDOM.render(
   <MyComponent name="Oscar" />,
-  document.getElementById('app')
+  container
 )
+
+/* I want update state when component recieve new props */
+setTimeout(() => {
+  ReactDOM.render(
+    <MyComponent name="Miguel" />,
+    container
+  )
+}, 2000)
