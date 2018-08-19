823ce6410dbd4e3ff39a93d8e6b4441603d78e95
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-08-19 12:35:21 -0500

    Higher-Order Components

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 
 const container = document.getElementById('app')
 
-const CountContext = React.createContext(0)
+const CountContext = React.createContext()
 
-/* Pure Component with UI */
 function HelloComponent (props) {
   return <CountContext.Consumer>
     {
@@ -15,80 +14,60 @@ function HelloComponent (props) {
   </CountContext.Consumer>
 }
 
-/* State component with Logic */
-class MyComponent extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { count: 0 }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  componentWillMount () {
-    console.log('El componente se va a montar!')
-  }
-
-  componentDidMount () {
-    console.log('El componente se ha montado!')
-    // document.addEventListener('click', this.handleClick)
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-    console.log('Componente recibe propiedades y estados', nextProps, nextState)
-
-    const { name } = this.props
-    const { count } = this.state
-    
-    /* Update state count after click  */
-    if (name === nextProps.name && count !== nextState.count)
-      return true
-    
-    /* Update name after 2 seconds */
-    if (name !== nextProps.name && count === nextState.count) {
-      this.state.count *= 2
-      return true
+function Counter (WrappedComponent) {
+  return class extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = { count: 0 }
+      this.handleClick = this.handleClick.bind(this)
+    }
+  
+    shouldComponentUpdate (nextProps, nextState) {
+      const { name } = this.props
+      const { count } = this.state
+      
+      /* Update state count after click  */
+      if (name === nextProps.name && count !== nextState.count)
+        return true
+      
+      /* Update name after 2 seconds */
+      if (name !== nextProps.name && count === nextState.count) {
+        this.state.count *= 2
+        return true
+      }
+      
+      return false
+    }
+  
+    handleClick () {
+      this.setState({
+        count: this.state.count + 1
+      })
     }
     
-    return false
-  }
-
-  getSnapshotBeforeUpdate (prevProps, prevState) {
-    console.log('Componente antes de actualizar', prevProps, prevState)
-  }
-
-  componentDidUpdate (prevProps, prevState) {
-    console.log('Componente ha sido actualizado', prevProps, prevState)
-  }
-
-  componentWillUnmount () {
-    // document.removeEventListener('click', this.handleClick)
-  }
-
-  handleClick () {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  
-  render () {
-    return <CountContext.Provider value={this.state.count}>
-      <HelloComponent
-        name={this.props.name}
-        count={this.state.count}
-        handleClick={this.handleClick}
-      />
-    </CountContext.Provider>
+    render () {
+      return <CountContext.Provider value={this.state.count}>
+        <WrappedComponent
+          name={this.props.name}
+          count={this.state.count}
+          handleClick={this.handleClick}
+        />
+      </CountContext.Provider>
+    }
   }
 }
 
+const CounterHOC = Counter(HelloComponent)
+
 ReactDOM.render(
-  <MyComponent name="Oscar" />,
+  <CounterHOC name="Oscar" />,
   container
 )
 
 /* I want update state when component recieve new props */
 setTimeout(() => {
   ReactDOM.render(
-    <MyComponent name="Miguel" />,
+    <CounterHOC name="Miguel" />,
     container
   )
 }, 2000)
